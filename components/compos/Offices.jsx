c8241fa4acade75cec09053d1bee92e83edb4785
Bello-Barry <79174154+Bello-Barry@users.noreply.github.com>
2024-01-03 12:28:37 +0100

    Update Offices.jsx

--- components/compos/Offices.jsx
+++ components/compos/Offices.jsx
@@ -21,18 +21,14 @@ const Offices = ({ invert = false, ...props }) => {
   return (
     <ul role="list" {...props}>
       <li>
-        <Office name="U.S.A" invert={invert}>
-          on bay mark
+        <Office name="brazzaville" invert={invert}>
+         Moungali
           <br />
-          Newtown city of Las Vegas
+          Rue lobi 29
         </Office>
       </li>
       <li>
-        <Office name="London" invert={invert}>
-          13 long Street
-          <br />
-          Downtown, Allyway
-        </Office>
+       
       </li>
     </ul>
   );
