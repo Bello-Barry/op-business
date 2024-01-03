ba7d573ee62e1e684f2872e015642f1ada6ec2e4
Bello-Barry <79174154+Bello-Barry@users.noreply.github.com>
2024-01-03 09:33:40 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -36,21 +36,19 @@ export default function Home() {
 			</FadeIn>
 			<ZoomIn delay={3}  scale={7} duration={5} >
 				<p className='text-lg font-display'>
-					<a href='https://x.com/anxelli_dev' target='_blank' rel='noopener noreferrer'>
-						<small>by</small> <strong>@barry_bello_dev</strong>
-					</a>
+					
+						<small>by</small> <strong>Op&business </strong>
+					
 				</p>
 				
 			</ZoomIn>
 
 			<FadeI className="max-w-3xl">
           <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
-            Make your online presence a reality with us!
+            Faites votre présence en ligne une réalité avec nous !
           </h1>
           <p className="mt-6 text-xl text-neutral-600">
-            We are a group of developer working at the intersection of design
-            and technology. It is a really busy intersection though — a lot of
-            our staff have been involved in hit and runs.
+            Nous sommes un groupe de développeurs travaillant à l'intersection du disign  et la technologie moderne.
           </p>
           </FadeI>
 		  <ZoomIn delay={3}  scale={7} duration={5} >
