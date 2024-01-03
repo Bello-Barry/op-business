5f8c690176657e6abdfe8a4a83bce83a915977ce
Bello-Barry <79174154+Bello-Barry@users.noreply.github.com>
2024-01-03 06:52:33 +0100

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -20,29 +20,14 @@ export default function About() {
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
         After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
+        <span className="font-medium">OP&BUSINESS</span>, est une agence spécialisée dans la création de sites web modernes et d’applications mobiles{" "}
+        <span className="font-medium">le marketing et le référencement. Nous vous accompagnons dans la conception, le développement, et la promotion de vos projets numériques.</span>.{" "}
+       
+        nous sommes à l’écoute de vos besoins et de vos attentes. Nous vous garantissons un travail de qualité, un respect des délais, et une satisfaction totale.
       </p>
 
       <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        <span className="italic">N’hésitez pas à nous contacter pour discuter de vos projets et obtenir un devis gratuit. Nous serons ravis de vous répondre et de vous accompagner.
       </p>
     </motion.section>
   );
