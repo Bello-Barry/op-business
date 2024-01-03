4938872c96fbbae1b03bd0985e82bae04ceffd90
Bello-Barry <79174154+Bello-Barry@users.noreply.github.com>
2024-01-03 13:01:38 +0100

    Update FooterNavigation.jsx

--- components/compos/FooterNavigation.jsx
+++ components/compos/FooterNavigation.jsx
@@ -7,7 +7,7 @@ const FooterNavigation = () => {
       <ul role="list" className="grid grid-cols-2 gap-8 sm:grid-cols-3">
         {navigation.map((item) => (
           <li key={item.title}>
-            <div className="font-display text-sm font-semibold tracking-wider text-neutral-950">
+            <div className="font-display text-sm font-semibold tracking-wider text-slate-400">
               {item.title}
             </div>
             <ul role="list" className="mt-4 text-sm text-neutral-700">
@@ -15,7 +15,7 @@ const FooterNavigation = () => {
                 <li key={link.title} className="mt-4">
                   <Link
                     href={link.href}
-                    className="transition hover:text-neutral-950"
+                    className="text-inherit transition hover:text-neutral-950"
                   >
                     {link.title}
                   </Link>
