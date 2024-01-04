9e7bc8009c530a9cb3deb148ab987b31aa4008c5
Bello-Barry <79174154+Bello-Barry@users.noreply.github.com>
2024-01-04 12:38:02 +0100

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -94,7 +94,7 @@ export default function Intro() {
         
  <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="https://www.facebook.com/Entrpreneuriat2.0?"
           target="_blank"
         >
        
@@ -105,7 +105,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="https://www.linkedin.com/in/bello-barry-13838b106"
           target="_blank"
         >
           <BsLinkedin />
@@ -113,7 +113,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+          href="https://github.com/Bello-Barry"
           target="_blank"
         >
           <FaGithubSquare />
