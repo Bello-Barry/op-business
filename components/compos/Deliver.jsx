ba138f4d72d4f76fa928c27e4322f10b4e17a998
Bello-Barry <79174154+Bello-Barry@users.noreply.github.com>
2024-01-04 11:15:36 +0100

    Update Deliver.jsx

--- components/compos/Deliver.jsx
+++ components/compos/Deliver.jsx
@@ -11,45 +11,45 @@ const Deliver = () => {
           À peu près à mi-chemin de la phase de construction, nous poussons chaque projet de 6
           semaines en raison d’un changement de{" "}
           <strong className="font-semibold text-neutral-950">
-            requirements
+            Exigences
           </strong>
-          . This allows us to increase the budget a final time before launch.
+          . Cela nous permet d’augmenter le budget une dernière fois avant le lancement.
         </p>
         <p>
-          Despite largely using pre-built components, most of the{" "}
-          <strong className="font-semibold text-neutral-950">progress</strong>{" "}
-          on each project takes place in the final 24 hours. The development
-          time allocated to each client is actually spent making augmented
-          reality demos that go viral on Twitter.
+          Bien qu’ils utilisent en grande partie des composants préfabriqués, la plupart des{" "}
+          <strong className="font-semibold text-neutral-950">progrès</strong>{" "}
+          sur chaque projet a lieu dans les dernières 24 heures. Le développement
+          le temps alloué à chaque client est en fait consacré à la réalisation d’augmentations
+          des démos de télé-réalité qui deviennent virales sur Twitter.
         </p>
         <p>
-          We ensure that the main pages of the site are{" "}
+          Nous veillons à ce que les pages principales du site soient{" "}
           <strong className="font-semibold text-neutral-950">
-            fully functional
+            Entièrement fonctionnel
           </strong>{" "}
-          at launch — the auxiliary pages will, of course, be lorem ipusm shells
-          which get updated as part of our exorbitant{" "}
+          Au lancement, les pages auxiliaires seront, bien sûr, des coquilles d’IPUSM de Lorem
+          qui sont mis à jour dans le cadre de notre{" "}
           <strong className="font-semibold text-neutral-950">
-            maintenance
+            entretien
           </strong>{" "}
-          retainer.
+          serviteur.
         </p>
       </div>
       <h3 className="mt-12 font-display text-base font-semibold text-neutral-950">
-        Included in this phase
+        Inclus dans cette phase
       </h3>
       <List>
         <ListItem title="Testing">
-          Our projects always have 100% test coverage, which would be impressive
-          if our tests weren’t as porous as a sieve.
+          Nos projets ont toujours une couverture de test de 100 %, ce qui serait impressionnant
+          si nos tests n’étaient pas aussi poreux qu’un tamis.
         </ListItem>
         <ListItem title="Infrastructure">
-          To ensure reliability we only use the best Digital Ocean droplets that
-          $4 a month can buy.
+          Pour garantir la fiabilité, nous n’utilisons que les meilleures gouttelettes Digital Ocean qui
+          4 $ par mois peuvent acheter.
         </ListItem>
         <ListItem title="Support">
-          Because we hold the API keys for every critical service your business
-          uses, you can expect a lifetime of support, and invoices, from us.
+          Parce que nous détenons les clés API pour chaque service critique de votre entreprise
+          utilisations, vous pouvez vous attendre à une vie d’assistance et de factures, de notre part.
         </ListItem>
       </List>
     </Section>
