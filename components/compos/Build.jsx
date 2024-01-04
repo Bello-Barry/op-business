6d78f62d0392f6de1dbc2f2ce1c8c52edf565ec2
Bello-Barry <79174154+Bello-Barry@users.noreply.github.com>
2024-01-04 11:31:06 +0100

    Update Build.jsx

--- components/compos/Build.jsx
+++ components/compos/Build.jsx
@@ -5,34 +5,34 @@ import Blockquote from "./Blockquote";
 
 const Build = () => {
   return (
-    <Section title="Build" image={{ src: imageLaptop, shape: 2 }}>
+    <Section title="Construire" image={{ src: imageLaptop, shape: 2 }}>
       <div className="space-y-6 text-base text-neutral-600">
         <p>
-          Based off of the discovery phase, we develop a comprehensive roadmap
-          for each product and start working towards delivery. The roadmap is an
-          intricately tangled mess of technical nonsense designed to drag the
-          project out as long as possible.
+          Sur la base de la phase de découverte, nous élaborons une feuille de route complète
+          pour chaque produit et commencer à travailler en vue de la livraison. La feuille de route est un
+          un enchevêtrement complexe d’absurdités techniques conçues pour faire traîner le
+          projeter le plus longtemps possible.
         </p>
         <p>
-          Each client is assigned a key account manager to keep lines of
-          communication open and obscure the actual progress of the project.
-          They act as a buffer between the client’s incessant nagging and the
-          development team who are hard at work scouring open source projects
-          for code to re-purpose.
+          Chaque client se voit attribuer un responsable grands comptes pour tenir les lignes de
+          communication ouverte et obscurcissent l’avancement réel du projet.
+          Ils agissent comme un tampon entre les harcèlements incessants du client et la
+          Une équipe de développement qui travaille d’arrache-pied pour éplucher des projets open source
+          pour que le code soit réutilisé.
         </p>
         <p>
-          Our account managers are trained to only reply to client emails after
-          9pm, several days after the initial email. This reinforces the general
-          aura that we are very busy and dissuades clients from asking for
-          changes.
+          Nos gestionnaires de compte sont formés pour ne répondre aux e-mails des clients qu’après
+          21h, plusieurs jours après l’e-mail initial. Cela renforce l’opinion générale
+          que nous sommes très occupés et dissuade les clients de demander
+          changements.
         </p>
       </div>
       <Blockquote
-        author={{ name: "Debra Fiscal", role: "CEO of Unseal" }}
+        author={{ name: "Barry bello", role: "CEO of Unseal" }}
         className="mt-12"
       >
-        Studio_clone were so regular with their progress updates we almost began
-        to think they were automated!
+        Nous étions si réguliers avec leurs mises à jour de progression que nous avons presque commencé
+        penser qu’ils étaient automatisés !
       </Blockquote>
     </Section>
   );
