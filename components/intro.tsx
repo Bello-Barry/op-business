5a0f1394e9c861b3f72c57657481dc9dfd6b163e
Bello-Barry <79174154+Bello-Barry@users.noreply.github.com>
2024-01-04 12:22:33 +0100

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -92,11 +92,16 @@ export default function Intro() {
         }}
       >
         
-
+ <a
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://linkedin.com"
+          target="_blank"
+        >
        
           
           <FaFacebook className="opacity-60 group-hover:translate-y-1 transition" />
         
+  </a>
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
