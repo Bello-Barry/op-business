a00cc8afc4d6f04b60a01eb1af90c50a1a25db90
Bello-Barry <79174154+Bello-Barry@users.noreply.github.com>
2024-01-04 11:19:17 +0100

    Update Cultures.jsx

--- components/compos/Cultures.jsx
+++ components/compos/Cultures.jsx
@@ -7,27 +7,27 @@ const Cultures = () => {
   return (
     <div className="mt-24 rounded-4xl bg-neutral-950 py-24 sm:mt-32 lg:mt-40 lg:py-32">
       <SectionIntro
-        eyebrow="Our culture"
-        title="Balance your passion with your passion for life."
+        eyebrow="Notre culture"
+        title="Équilibrez votre passion avec votre passion pour la vie."
         invert
       >
         <p>
-          We are a group of like-minded people who share the same core values.
+          Nous sommes un groupe de personnes partageant les mêmes idées et les mêmes valeurs fondamentales.
         </p>
       </SectionIntro>
       <Container className="mt-16">
         <GridList>
-          <GridListItem title="Loyalty" invert>
-            Our team has been with us since the beginning because none of them
-            are allowed to have LinkedIn profiles.
+          <GridListItem title="Loyauté" invert>
+            Notre équipe est avec nous depuis le début parce qu’aucun d’entre eux n’a
+            sont autorisés à avoir des profils LinkedIn.
           </GridListItem>
-          <GridListItem title="Trust" invert>
-            We don’t care when our team works just as long as they are working
-            every waking second.
+          <GridListItem title="Confiance" invert>
+            Nous ne nous soucions pas de savoir si notre équipe travaille aussi longtemps qu’elle travaille
+            chaque seconde d’éveil.
           </GridListItem>
           <GridListItem title="Compassion" invert>
-            You never know what someone is going through at home and we make
-            sure to never find out.
+            On ne sait jamais ce que quelqu’un vit à la maison et nous faisons
+            sûr de ne jamais le savoir.
           </GridListItem>
         </GridList>
       </Container>
